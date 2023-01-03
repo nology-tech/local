044b797acea9a206bca2fa41e44b45b7ce72e4ef
Elena Ristea <Elena.Ristea@nology.io>
2023-01-03 15:15:43 +0000

    added styles to description

--- src/pages/Enter/Enter.jsx
+++ src/pages/Enter/Enter.jsx
@@ -1,20 +1,26 @@
 import Layout from "../../components/Layout/Layout";
 import { Link } from "react-router-dom";
 import Button from "../../components/Button/Button";
+import "./Enter.scss";
+import logo from "../../assets/images/AppLogo.svg";
 
 const Enter = () => {
   return (
     <Layout>
-      <div>
-        <img className="logo" src="" alt="App Logo" />
-        <h1>LOCAL</h1>
-        <p>Bringing local business to local people</p>
-        <Link to="/enter/signin">
-          <Button buttonText={"Sign in"} />
-        </Link>
-        <Link to="/enter/signup">
-          <Button buttonText={"Sign up"} />
-        </Link>
+      <div className="enter">
+        <div className="enter__description">
+          <img className="enter__logo" src={logo} alt="App Logo" />
+          <h1>LOCAL</h1>
+          <p>Bringing local business to local people</p>
+        </div>
+        <div className="enter__buttons">
+          <Link to="/enter/signin">
+            <Button buttonText={"Sign in"} />
+          </Link>
+          <Link to="/enter/signup">
+            <Button buttonText={"Sign up"} />
+          </Link>
+        </div>
       </div>
     </Layout>
   );
