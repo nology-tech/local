bf0168583529e3cac29ce8f66555aadf70581211
Elena Ristea <Elena.Ristea@nology.io>
2023-01-03 11:48:27 +0000

    added login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import "./styles/main.scss";
+import Enter from "./pages/Enter/Enter";
 
 const App = () => {
   return (
     <Routes>
       <Route path="*" element={<Home />} />
+      <Route path="/enter" element={<Enter />} />
     </Routes>
   );
 };
---
+++ src/pages/Enter/Enter.jsx
@@ -0,0 +1,17 @@
+import Layout from "../../components/Layout/Layout";
+
+const Enter = () => {
+  return (
+    <Layout>
+      <div>
+        <img className="logo" src="" alt="App Logo" />
+        <h1>LOCAL</h1>
+        <p>Bringing local business to local people</p>
+        <button>Sign in</button>
+        <button>Sign up</button>
+      </div>
+    </Layout>
+  );
+};
+
+export default Enter;
