588411b72d79815c95731c46ec504bc9e15a59d4
Justin Morris <85644773+JustinDotMorris@users.noreply.github.com>
2023-01-03 16:31:25 +0000

    13 show mock data favourites (#3)
    Co-authored-by: sophharris1998 <117087805+sophharris1998@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Favourites from "./pages/Favourites/Favourites";
 import "./styles/main.scss";
 
 const App = () => {
   return (
     <Routes>
       <Route path="*" element={<Home />} />
+      <Route path="/favourites" element={<Favourites />} />
     </Routes>
   );
 };
