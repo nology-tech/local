c40a0f526b560a329f42ce8367b25113960e3bae
Dan Forder <danfrdr@gmail.com>
2023-01-04 15:21:48 +0000

    add firestore user methods

---
+++ src/api/userService.js
@@ -0,0 +1,48 @@
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+} from "@firebase/auth";
+import { doc, setDoc } from "@firebase/firestore";
+import { auth, db } from "../firebase";
+
+export const registerUser = async (
+  email,
+  password,
+  firstName,
+  lastName,
+  username
+) => {
+  const auth = getAuth();
+
+  try {
+    const { user } = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+
+    const docRef = doc(db, "users", user.uid);
+
+    await setDoc(docRef, {
+      id: auth.currentUser.uid,
+      firstName,
+      lastName,
+      username,
+      email,
+      favourites: [],
+    });
+  } catch ({ code, message }) {
+    console.error(`Error code: ${code}. Error message: ${message}`);
+  }
+};
+
+export const loginUser = async (email, password) => {
+  try {
+    // sign in with firebase
+    await signInWithEmailAndPassword(auth, email, password);
+    // navigate to the dashboard
+  } catch (error) {
+    console.log({ error });
+  }
+};
