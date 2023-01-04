c60773d6391ca0c0c376394a70fb98b94a69c209
Dan Forder <danfrdr@gmail.com>
2023-01-04 14:27:45 +0000

    add mock user information

---
+++ src/data/mockUser.js
@@ -0,0 +1,15 @@
+const mockUser = {
+  id: 123456,
+  firstName: "Barry",
+  lastName: "Smith",
+  username: "TheOG",
+  email: "barrysmith@fake-emails.com",
+  favourites: [
+    { id: "63acbe82e9f5e8160675818d", slug: "medicine-bakery" },
+    { id: "63acbe82e9f5e816067581b3", slug: "la-m-c" },
+    { id: "63acbe82e9f5e816067581c4", slug: "the-indian-streatery" },
+    { id: "63acbe82e9f5e816067581bc", slug: "the-lost-and-found-birmingham" },
+  ],
+};
+
+export default mockUser;
