771fb0f785c1b67d3dbc750a3a5db97b9c567343
Justin Morris <85644773+JustinDotMorris@users.noreply.github.com>
2023-01-05 21:32:05 +0000

    Testing Progress made

--- src/components/BusinessCard/BusinessCard.jsx
+++ src/components/BusinessCard/BusinessCard.jsx
@@ -64,7 +64,6 @@ const BusinessCard = () => {
           onClick={() => setActiveDetails(null)}
         />
       )}
-      ;
     </>
   );
 };
--- src/components/BusinessCard/BusinessCard.test.jsx
+++ src/components/BusinessCard/BusinessCard.test.jsx
@@ -1,8 +1,18 @@
-import FavouritesCard from "./FavouritesCard";
+import BusinessCard from "./BusinessCard";
 import { customRender } from "../../utils/testUtils";
+import { Screen } from "@testing-library/react";
 
 it("should render the text on the display", () => {
-  const { container } = customRender(<FavouritesCard />);
+  const { container } = customRender(<BusinessCard />);
 
   expect(container).toMatchSnapshot();
 });
+
+It("should check if the click of the details button shows the details pane"),
+  () => {
+    const { container } = customRender(<BusinessCard />);
+    const button = screen.getByRole("button");
+
+    userEvent.click(button);
+    expect(container);
+  };
