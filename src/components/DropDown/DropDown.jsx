346baafc7f0290b61503bcfb0b622ce0a762e4e4
george Swift <george.swift@nology.io>
2023-01-05 12:39:30 +0000

    added state for active dropdowna nd location updated

--- src/components/DropDown/DropDown.jsx
+++ src/components/DropDown/DropDown.jsx
@@ -1,26 +1,76 @@
 import "./DropDown.scss";
+import triangle from "../../assets/icons/triangleUpsideDown.svg";
+
+import { useState } from "react";
 
 const DropDown = () => {
+  // state to change whether drop down is displayed or not - changes class of image to rotate the arrow and class of primary div to change borders
+  const [dropdownActive, setDropdownActive] = useState(false);
+  // state to track the active location selected - This defaults to all so that without any option being clicked all is displayed as the text
+  const [activeLocation, setActiveLocation] = useState("All");
+
+  const handleDropDownActive = () => {
+    console.log("hi");
+    setDropdownActive(!dropdownActive);
+  };
+  const handleActiveLocation = (location) => {
+    console.log(location);
+    setActiveLocation(`${location}`);
+  };
+
+  const applyActiveClassDiv = dropdownActive
+    ? "dropdown__item dropdown__item--primary dropdown__item--primary--active"
+    : "dropdown__item dropdown__item--primary";
+
+  const applyActiveClassArrow = dropdownActive
+    ? "dropdown__arrow arrow-active"
+    : "dropdown__arrow";
+
   return (
-    <select name="dropdown" id="locations" className="dropdown">
-      <option value="all" className="dropdown__option">
-        All
-      </option>
-      <option value="london" className="dropdown__option">
-        London
-      </option>
-      <option value="liverpool" className="dropdown__option">
-        Liverpool
-      </option>
-    </select>
+    <div className="dropdown">
+      <div role="select" tabIndex={0} className={applyActiveClassDiv}>
+        <p className="dropdown__item-name dropdown__item-name--primary">
+          {activeLocation}
+        </p>
+        <div
+          className={"dropdown__image-container"}
+          role="button"
+          tabIndex={0}
+          onClick={handleDropDownActive}
+        >
+          <img className={applyActiveClassArrow} src={triangle} alt="" />
+        </div>
+      </div>
+      {dropdownActive && (
+        <>
+          <div
+            role="option"
+            tabIndex={0}
+            className="dropdown__item dropdown__item--secondary"
+            onClick={() => handleActiveLocation("liverpool")}
+          >
+            <p className="dropdown__item-name">liverpool</p>
+          </div>
+          <div className="dropdown__item dropdown__item--secondary">
+            <p className="dropdown__item-name">london</p>
+          </div>
+          <div className="dropdown__item dropdown__item--secondary">
+            <p className="dropdown__item-name">edinburgh</p>
+          </div>
+        </>
+      )}
+    </div>
   );
 };
 export default DropDown;
 
-// note to remove the route for the drop down and the import of it from there once done.
-// need to update inital button name to be that of the selected location.
-// need state that is changed on click that hides ie showMoreContent && {dropdown contet div}
-// state on click needs to change img to transform 180 by application of class + change dropdown link primary to use the hover class variant of it.
+//steps to do next
 
-// 1. state needs to change the class applied to the first link and also icon (onclick)
-// 2. do props replacement for dynamic dropdown items list use - integrate api
+// Filter locaiton results arry [1,2,3,4,45,5,67,]
+// user click on 3
+// 3 then is placed where all currently is, function the does resultsarray.push("all")
+// 4. if i then clicked 4- resultsarray.shift(3)
+// 4. if option clicked = all then remove all from results arry and put current value into array.
+
+// replace hardcoded shit with props.
+//  split the cuurrent component into container - default value shown ie all + an imported component with props -
