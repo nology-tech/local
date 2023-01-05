3b90cdc6b4b8acb179b0d28003e686cf6fcee032
Justin Morris <85644773+JustinDotMorris@users.noreply.github.com>
2023-01-05 10:05:34 +0000

    Moved data to one file and shown on screen

--- src/components/FavDetails/FavDetails.jsx
+++ src/components/FavDetails/FavDetails.jsx
@@ -1,57 +1,23 @@
 import "./FavDetails.scss";
 import Button from "../Button/Button";
-const FavDetails = ({ onClick }) => {
-  const arrayDetails = [
-    {
-      id: "0",
-      name: "1Ruebens Coffee Shop",
-      paragraph:
-        "Mom and Pop chain serving sandwhiches and stuff. Poppy seed bagels are nice as are seaseme seed bagel e.t.c.",
-      weekdayOpening: "MON-FRI : 9:00 - 19:00",
-      weekendOpening: "Sat : 10:00 - 15:00",
-      place: "123 Fake Street, Covent Garden, London",
-      price: "££",
-      image: " IMAGE INSERT HERE ",
-    },
-    {
-      id: "1",
-      name: "2Ruebens Hot Choccy Shop",
-      paragraph:
-        "Mom and Pop chain serving sandwhiches and stuff. Poppy seed bagels are nice as are seaseme seed bagel e.t.c.",
-      weekdayOpening: "MON-FRI : 9:00 - 19:00",
-      weekendOpening: "Sat : 10:00 - 15:00",
-      place: "123 Fake Street, Covent Garden, London",
-      price: "££",
-      image: " IMAGE INSERT HERE ",
-    },
-    {
-      id: "2",
-      name: "3Ruebens Shop",
-      paragraph:
-        "Mom and Pop chain serving sandwhiches and stuff. Poppy seed bagels are nice as are seaseme seed bagel e.t.c.",
-      weekdayOpening: "MON-FRI : 9:00 - 19:00",
-      weekendOpening: "Sat : 10:00 - 15:00",
-      place: "123 Fake Street, Covent Garden, London",
-      price: "££",
-      image: " IMAGE INSERT HERE ",
-    },
-  ];
+// import { useEffect } from "react";
 
-  const showDetails = arrayDetails.map((modal) => {
+const FavDetails = ({ onClick, favArray }) => {
+  const showDetails = favArray.map((modal) => {
     return (
       <div className="favDetails" key={modal.id}>
         <div className="favDetails__Container">
           <div className="favDetails__Header">
             <h2>{modal.name}</h2>
-            <Button buttonText="X" buttonName="Close" onClick={onClick} />
+            <Button buttonText="✖" buttonName="Close" onClick={onClick} />
           </div>
-          <p>{modal.paragraph}</p>
+          <p>{modal.description}</p>
           <p>{modal.weekdayOpening}</p>
           <p>{modal.weekendOpening}</p>
-          <p>{modal.place}</p>
+          <p>{modal.address}</p>
           <p>{modal.price}</p>
           <p>{modal.image}</p>
-          <div>
+          <div className="favDetails__SaveBtnContainer">
             <Button buttonText="Save" buttonName="Save" />
           </div>
         </div>
--- src/components/FavouritesCard/FavouritesCard.jsx
+++ src/components/FavouritesCard/FavouritesCard.jsx
@@ -6,40 +6,40 @@ import "./FavouritesCard.scss";
 const FavouritesCard = () => {
   const [showDetails, setShowDetails] = useState(false);
 
-  const arrayFav = [
+  const favArray = [
     {
       id: "0",
       name: "Smith & Sons",
-      paragraph: "Mom and Pop chain serving sandwhiches",
+      description: "Mom and Pop chain serving sandwhiches",
       weekdayOpening: "MON-FRI : 9:00 - 19:00",
       weekendOpening: "Sat : 10:00 - 15:00",
-      price: "££",
-      place: "32 Camden Hill, Eeling, London",
+      address: "32 Camden Hill, Eeling, London",
+      price: "£££££",
       image: " IMAGE INSERT HERE ",
     },
     {
       id: "1",
       name: "Dirty Onion",
-      paragraph: "Irish Bar, Great guines on tap. Live music on saturdays.",
+      description: "Irish Bar, Great guines on tap. Live music on saturdays.",
       weekdayOpening: "MON-FRI : 9:00 - 19:00",
       weekendOpening: "Sat : 10:00 - 15:00",
-      place: "36 Lamden Hill, Eeling, London",
-      price: "££",
+      address: "36 Lamden Hill, Eeling, London",
+      price: "£",
       image: " IMAGE INSERT HERE ",
     },
     {
       id: "2",
       name: "Gunners art gallery",
-      paragraph: "Mom and Pop chain serving sandwhiches",
+      description: "I SELL ART & STUFF",
       weekdayOpening: "MON-FRI : 9:00 - 19:00",
       weekendOpening: "Sat : 10:00 - 15:00",
-      place: "112 Backden Hill, Eeling, London",
-      price: "££",
+      address: "112 Backden Hill, Eeling, London",
+      price: "£££",
       image: " IMAGE INSERT HERE ",
     },
   ];
 
-  const newFav = arrayFav.map((card) => {
+  const newFav = favArray.map((card) => {
     return (
       <div key={card.id}>
         <h2>{card.name}</h2>
@@ -51,9 +51,7 @@ const FavouritesCard = () => {
             buttonText="Details"
             onClick={() => setShowDetails(!showDetails)}
           />
-          {showDetails && (
-            <FavDetails onClick={() => setShowDetails(!showDetails)} />
-          )}
+          {showDetails && <FavDetails favArray={favArray} />}
         </div>
       </div>
     );
