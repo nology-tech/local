a83fa30dede49cdaedd64ad9aa63f28b0f69654d
Elena Ristea <Elena.Ristea@nology.io>
2023-01-05 16:08:36 +0000

    added style to menu layout

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -25,46 +25,54 @@ const Menu = ({ toggleMenu }) => {
           />
         </div>
         <div className="menu-home">
-          <div className="menu__page">
-            <img className="menu-logo" src={home} alt="Home Logo" />
-            <h2 className="">Home</h2>
-            <img className="menu-arrow" src={arrow} alt="Arrow" />
-          </div>
+          <Link className="menu__link" to="/home">
+            <div className="menu__page">
+              <img className="menu-logo" src={home} alt="Home Logo" />
+              <h2>Home</h2>
+              <img className="menu-arrow" src={arrow} alt="Arrow" />
+            </div>
+          </Link>
         </div>
         <div className="menu-search">
-          <div className="menu__page ">
-            <img className="menu-logo" src={discover} alt="Discover Logo" />
-            <h2>Discover</h2>
-            <img className="menu-arrow" src={arrow} alt="Arrow" />
-          </div>
-          <div className="menu__page">
-            <img className="menu-logo" src={favourites} alt="Home Logo" />
-            <h2>Favourites</h2>
-            <Link to="/favourites">
+          <Link className="menu__link" to="/discover">
+            <div className="menu__page ">
+              <img className="menu-logo" src={discover} alt="Discover Logo" />
+              <h2>Discover</h2>
               <img className="menu-arrow" src={arrow} alt="Arrow" />
-            </Link>
-          </div>
+            </div>
+          </Link>
+          <Link className="menu__link" to="/favourites">
+            <div className="menu__page">
+              <img className="menu-logo" src={favourites} alt="Home Logo" />
+              <h2>Favourites</h2>
+              <img className="menu-arrow" src={arrow} alt="Arrow" />
+            </div>
+          </Link>
         </div>
         <div className="menu-info">
-          <div className="menu__page">
-            <img className="menu-logo" src={account} alt="Account Logo" />
-            <h2>Manage Account</h2>
-            <img className="menu-arrow" src={arrow} alt="Arrow" />
-          </div>
-          <div className="menu__page">
-            <img className="menu-logo" src={about} alt="About Logo" />
-            <h2>About</h2>
-            <img className="menu-arrow" src={arrow} alt="Arrow" />
-          </div>
+          <Link className="menu__link" to="/account">
+            <div className="menu__page">
+              <img className="menu-logo" src={account} alt="Account Logo" />
+              <h2>Manage Account</h2>
+              <img className="menu-arrow" src={arrow} alt="Arrow" />
+            </div>
+          </Link>
+          <Link className="menu__link" to="/about">
+            <div className="menu__page">
+              <img className="menu-logo" src={about} alt="About Logo" />
+              <h2>About</h2>
+              <img className="menu-arrow" src={arrow} alt="Arrow" />
+            </div>
+          </Link>
         </div>
         <div className="menu-contact">
-          <div className="menu__page">
-            <img className="menu-logo" src={contact} alt="Contact Logo" />
-            <h2>Contact</h2>
-            <Link to="/contact">
+          <Link className="menu__link" to="/contact">
+            <div className="menu__page">
+              <img className="menu-logo" src={contact} alt="Contact Logo" />
+              <h2>Contact</h2>
               <img className="menu-arrow" src={arrow} alt="Arrow" />
-            </Link>
-          </div>
+            </div>
+          </Link>
         </div>
       </div>
     </>
