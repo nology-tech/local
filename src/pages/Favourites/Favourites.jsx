598cd4e87eb9c281a0ecb1c96b98dce9ad18dc8f
Edgars <Edgars.Kaufmanis@nology.io>
2023-01-06 16:53:26 +0000

    removed opacity

--- src/pages/Favourites/Favourites.jsx
+++ src/pages/Favourites/Favourites.jsx
@@ -1,6 +1,7 @@
 import BusinessCard from "../../components/BusinessCard/BusinessCard";
 import Layout from "../../components/Layout/Layout";
 import favArray from "../../data/favourites/favourites";
+import DropDown from "../../components/DropDown/DropDown";
 
 const Favourites = () => {
   const optionsArr = [
@@ -19,6 +20,7 @@ const Favourites = () => {
 
   return (
     <Layout>
+      <DropDown onChange={onChange} options={optionsArr} />
       <BusinessCard businessCardArray={favArray} />
     </Layout>
   );
