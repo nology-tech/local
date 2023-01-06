e1236fcd48854121ae5ce62aa09ca73525a2cc6f
Edgars <Edgars.Kaufmanis@nology.io>
2023-01-06 17:00:09 +0000

    testing updated

--- src/components/DropDown/DropDown.test.jsx
+++ src/components/DropDown/DropDown.test.jsx
@@ -3,15 +3,17 @@ import { customRender } from "../../utils/testUtils";
 import { screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
+const mockData = ["London", "Newcastle"];
+
 it("should render the dropdown", () => {
-  const { container } = customRender(<DropDown />);
+  const { container } = customRender(<DropDown options={mockData} />);
 
   expect(container).toMatchSnapshot();
 });
 
 it("should call the function when user selects an option from the dropdown menu", () => {
   const mockFunction = jest.fn();
-  const mockData = ["London", "Newcastle"];
+
   customRender(<DropDown onChange={mockFunction} options={mockData} />);
 
   const dropDown = screen.getByRole("select");
