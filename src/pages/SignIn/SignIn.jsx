c54250261391f00200cb0bb5c1bdd8e779d1e01c
katherine <pv.katherine7@gmail.com>
2023-01-06 14:12:40 +0000

    additions to sign in page

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,8 +1,36 @@
 import Layout from "../../components/Layout/Layout";
+import logo from "../../assets/images/AppLogo.svg";
+import TextField from "../../components/TextField/TextField";
+import Button from "../../components/Button/Button";
+import "./SignIn.scss";
+
 const SignIn = () => {
   return (
     <Layout>
-      <p>Sign In</p>
+      <div className="sign-in">
+        <div className="sign-in__intro">
+          <img className="sign-in__logo" src={logo} alt="App Logo" />
+          <h1>LOCAL</h1>
+          <p>Bringing local business to local people</p>
+        </div>
+        <div className="sign-in__input-field">
+          <TextField
+            uniqueInputFieldName="signinUserName"
+            inputType="text"
+            labelHeader="Username"
+          />
+          <TextField
+            uniqueInputFieldName="signinPassword"
+            inputType="text"
+            labelHeader="Password"
+          />
+          <div className="sign-in__buttons">
+            <Button buttonText={"Sign in"} buttonName="signin" />
+            <Button buttonText={"Sign in with Google"} buttonName="signin" />
+          </div>
+        </div>
+      </div>
+      {/* </div> */}
     </Layout>
   );
 };
