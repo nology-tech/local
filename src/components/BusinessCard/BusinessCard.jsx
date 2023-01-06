0e51c26061592ceeaf61bcb7c473fe993623424f
george Swift <george.swift@nology.io>
2023-01-06 15:43:53 +0000

    branch and scss file creation, classnames added

--- src/components/BusinessCard/BusinessCard.jsx
+++ src/components/BusinessCard/BusinessCard.jsx
@@ -1,16 +1,17 @@
 import { useState } from "react";
 import Button from "../Button/Button";
 import DetailsPane from "../DetailsPane/DetailsPane";
+import "./BusinessCard.scss";
 
 const BusinessCard = ({ businessCardArray }) => {
   const [activeDetails, setActiveDetails] = useState(null);
 
   const showCards = businessCardArray.map((card) => {
     return (
-      <div key={card.id}>
-        <h2>{card.name}</h2>
-        <p>{card.description}</p>
-        <p>{card.address}</p>
+      <div className="card" key={card.id}>
+        <h2 className="card__heading">{card.name}</h2>
+        <p className="card__text">{card.description}</p>
+        <p className="card__text card__text--adress">{card.address}</p>
         <div>
           <Button
             buttonName="details"
