6ea6815cdac9072100f98cc53b3def871374820b
Justin Morris <85644773+JustinDotMorris@users.noreply.github.com>
2023-01-09 10:12:32 +0000

    Completed testing

--- src/components/Header/Header.test.jsx
+++ src/components/Header/Header.test.jsx
@@ -1,7 +1,11 @@
 import Header from "./Header";
 import { customRender } from "../../utils/testUtils";
+import { screen } from "@testing-library/react";
 
 it("should render the header on the page", () => {
   const { container } = customRender(<Header />);
+
+  const textField = screen.getByText("LOCAL");
+
   expect(container).toMatchSnapshot();
 });
