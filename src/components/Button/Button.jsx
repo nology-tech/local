a914c41a8fb7c33a4f5c930cdc23d9f2d020c8d6
Justin Morris <85644773+JustinDotMorris@users.noreply.github.com>
2023-01-09 17:57:38 +0000

    styled some buttons for map navigation and details page

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,20 @@
 import "./Button.scss";
 
-const Button = ({ buttonText, buttonName, onClick }) => {
+const Button = ({
+  buttonText,
+  buttonName,
+  onClick,
+  buttonImgUrl,
+  buttonImgAlt,
+  buttonImgClass,
+}) => {
   let buttonClass = `button button--${buttonName}`;
+  let buttonImageName = `button--image--${buttonImgClass}`;
 
   return (
     <button className={buttonClass} onClick={onClick}>
       {buttonText}
+      <img className={buttonImageName} src={buttonImgUrl} alt={buttonImgAlt} />
     </button>
   );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,28 @@
+import Button from "../../components/Button/Button";
 import Layout from "../../components/Layout/Layout";
+import discoverIcon from "../../assets/icons/discoverIcon.svg";
+import plusIcon from "../../assets/icons/plusIcon.svg";
+import minusIcon from "../../assets/icons/minusIcon.svg";
 
 const Home = () => {
   return (
     <Layout isWithMenu={true}>
+      <Button
+        buttonName={"map-navigation"}
+        buttonImgUrl={plusIcon}
+        buttonImgClass={"discover-icon"}
+      />
+      <Button
+        buttonName={"map-navigation"}
+        buttonImgUrl={minusIcon}
+        buttonImgClass={"discover-icon"}
+      />
+      <Button
+        // buttonText={"D"}
+        buttonName={"map-navigation"}
+        buttonImgUrl={discoverIcon}
+        buttonImgClass={"discover-icon"}
+      />
       <h1>Page Heading</h1>
       <h2>Section Heading</h2>
       <h3>Panel Heading</h3>
