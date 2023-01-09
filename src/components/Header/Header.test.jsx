c5db1a7de994149fae0226ada07ef943188e58a7
Justin Morris <85644773+JustinDotMorris@users.noreply.github.com>
2023-01-09 10:24:46 +0000

    fixed testing error

--- src/components/Header/Header.test.jsx
+++ src/components/Header/Header.test.jsx
@@ -3,9 +3,9 @@ import { customRender } from "../../utils/testUtils";
 import { screen } from "@testing-library/react";
 
 it("should render the header on the page", () => {
-  const { container } = customRender(<Header />);
+  customRender(<Header />);
 
   const textField = screen.getByText("LOCAL");
 
-  expect(container).toMatchSnapshot();
+  expect(textField).toBeInTheDocument();
 });
