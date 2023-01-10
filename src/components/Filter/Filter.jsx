877fe4d7f6c9ad2f69fa3b133d8fcdb04f768eb9
ellaellac <ellachoywy@gmail.com>
2023-01-10 09:20:50 +0000

    filters working together

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,12 +1,14 @@
 import "./Filter.scss";
 import Button from "../Button/Button";
 import favArray from "../../data/favourites";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Filter = () => {
   const [foodDrinkFilter, setFoodDrinkFilter] = useState(false);
   const [cultureFilter, setCultureFilter] = useState(false);
   const [otherFilter, setOtherFilter] = useState(false);
+
+  const [allCardsArr, setallCardsArr] = useState(favArray);
 
   const handleClickFoodDrink = () => {
     setFoodDrinkFilter(!foodDrinkFilter);
@@ -20,22 +22,28 @@ const Filter = () => {
     setOtherFilter(!otherFilter);
   };
 
-  const favFilter = favArray.filter((fav) => {
-    if (foodDrinkFilter === true) {
-      return fav.category.includes("Food & Drink");
-    } else if (cultureFilter === true) {
-      return fav.category.includes("Culture");
-    } else if (otherFilter === true) {
-      return fav.category.includes("Other");
-    }
-  });
+  const filteredCard = (foodDrinkFilter, cultureFilter, otherFilter) => {
+    console.log(foodDrinkFilter, cultureFilter, otherFilter);
+    const filteredArr = favArray.filter(
+      (fav) =>
+        (foodDrinkFilter ? fav.category.includes("Food & Drink") : true) &&
+        (cultureFilter ? fav.category.includes("Culture") : true) &&
+        (otherFilter ? fav.category.includes("Other") : true)
+    );
+    return setallCardsArr(filteredArr);
+  };
 
-  const FavCardJSX = favFilter.map((card) => {
+  useEffect(() => {
+    filteredCard(foodDrinkFilter, cultureFilter, otherFilter);
+  }, [foodDrinkFilter, cultureFilter, otherFilter]);
+
+  const FavCardJSX = allCardsArr.map((card) => {
     return (
       <div key={card.id}>
         <h2>{card.name}</h2>
         <p>{card.description}</p>
         <p>{card.address}</p>
+        <p>{card.category}</p>
       </div>
     );
   });
