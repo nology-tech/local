79c4d9cf42886e9324c82d63807e468b34a163e7
Edgars <Edgars.Kaufmanis@nology.io>
2023-01-10 11:38:08 +0000

    added scroll functionality to filter list

--- src/pages/Favourites/Favourites.jsx
+++ src/pages/Favourites/Favourites.jsx
@@ -1,7 +1,6 @@
 import FavouritesCard from "../../components/FavouritesCard/FavouritesCard";
 import Layout from "../../components/Layout/Layout";
 import DropDown from "../../components/DropDown/DropDown";
-import Filter from "../../components/Filter/Filter";
 
 const Favourites = () => {
   const optionsArr = [
@@ -21,7 +20,7 @@ const Favourites = () => {
   return (
     <Layout>
       <DropDown onChange={onChange} options={optionsArr} />
-      <Filter />
+
       <FavouritesCard />
     </Layout>
   );
