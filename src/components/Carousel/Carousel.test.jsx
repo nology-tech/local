21071fa25f82337f364dec4ecef1f7af32247219
katherine <pv.katherine7@gmail.com>
2023-01-10 16:14:57 +0000

    test for carousel component

--- src/components/Carousel/Carousel.test.jsx
+++ src/components/Carousel/Carousel.test.jsx
@@ -2,17 +2,10 @@ import { customRender } from "../../utils/testUtils";
 import Carousel from "../Carousel/Carousel";
 
 it("should render the text on the display", () => {
-  const { container } = customRender(<Carousel />);
-
-  expect(container).toMatchSnapshot();
-});
-
-it("should recieve an array of components and render them on the screen", () => {
-  const mockComponents = ["<p>test</p>", "<p>other test</p>"];
+  const mockComponents = [<p>test</p>, <p>other test</p>];
   const { container } = customRender(
     <Carousel componentToDisplay={mockComponents} />
   );
-  mockComponents.forEach((element) => {
-    expect(container).toBeInTheDocument();
-  });
+
+  expect(container).toMatchSnapshot();
 });
