fcbe6b4751aae799571e8668aefa01955b167b0e
Elena Ristea <Elena.Ristea@nology.io>
2023-01-10 11:55:03 +0000

    added carousel on home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,11 @@
 import Layout from "../../components/Layout/Layout";
+import { useContext } from "react";
+import UserContext from "../../context/UserContext";
+import Carousel from "../../components/Carousel/Carousel";
+import BusinessCard from "../../components/BusinessCard/BusinessCard";
 
 const Home = () => {
+  const { user } = useContext(UserContext);
   return (
     <Layout isWithMenu={true}>
       <h1>Page Heading</h1>
@@ -12,6 +17,11 @@ const Home = () => {
         maxime tempora, temporibus beatae voluptas repellat rerum. Dignissimos,
         necessitatibus.
       </p>
+      <Carousel
+        componentToDisplay={
+          <BusinessCard businessCardArray={user.favourites} />
+        }
+      />
     </Layout>
   );
 };
