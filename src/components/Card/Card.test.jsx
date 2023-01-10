30248666a0b829e455e9b9aed9f67da6609d7b7c
GeorgeSwift16 <117093837+GeorgeSwift16@users.noreply.github.com>
2023-01-10 11:48:12 +0000

    23 favourites card design (#19)

--- src/components/Card/Card.test.jsx
+++ src/components/Card/Card.test.jsx
@@ -1,20 +1,18 @@
 import { customRender } from "../../utils/testUtils";
 import { screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import BusinessCard from "./BusinessCard";
+import Card from "./Card";
 import favArray from "../../data/favourites/favourites";
 
 it("should render the text on the display", () => {
-  const { container } = customRender(
-    <BusinessCard businessCardArray={favArray} />
-  );
+  const { container } = customRender(<Card cardArray={favArray} />);
 
   expect(container).toMatchSnapshot();
 });
 
 it("should check if the click of the details button shows the details pane", () => {
   //load the page
-  customRender(<BusinessCard businessCardArray={favArray} />);
+  customRender(<Card cardArray={favArray} />);
   //get the button
   const button = screen.getAllByRole("button");
   //on click of the button
