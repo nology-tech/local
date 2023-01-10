2db71d638cf84cd062f15747fb3b6069dcec1d14
george Swift <george.swift@nology.io>
2023-01-10 11:20:18 +0000

    updated mockuser data categories for testing

--- src/pages/Favourites/Favourites.test.jsx
+++ src/pages/Favourites/Favourites.test.jsx
@@ -1,6 +1,5 @@
 import { customRender } from "../../utils/testUtils";
 import Favourites from "./Favourites";
-
 it("should render the text on the display", () => {
   const { container } = customRender(<Favourites />);
 
