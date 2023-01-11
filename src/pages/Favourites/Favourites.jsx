e0ab4a1dc2384bf368e467df0bf5f5954ed99757
george Swift <george.swift@nology.io>
2023-01-11 10:45:17 +0000

    setup onclick, created func to update array, started func to pass to FB

--- src/pages/Favourites/Favourites.jsx
+++ src/pages/Favourites/Favourites.jsx
@@ -3,13 +3,12 @@ import Layout from "../../components/Layout/Layout";
 import DropDown from "../../components/DropDown/DropDown";
 import { useContext } from "react";
 import UserContext from "../../context/UserContext";
-
+import { handleUserFavouritesUpdate } from "../../api/userService";
 const Favourites = () => {
   const { user } = useContext(UserContext);
   if (user === null) {
     return <p>loading...</p>;
   }
-
   const optionsArr = [
     "All",
     "London",
@@ -20,6 +19,15 @@ const Favourites = () => {
     "Liverpool",
   ];
 
+  const handleRemoveFavourites = (cardId) => {
+    let localUserFavouritesArray = user.favourites;
+
+    const updatedUserFavourites = localUserFavouritesArray.filter(
+      (element) => element._id !== cardId
+    );
+    handleUserFavouritesUpdate(updatedUserFavourites, user.id);
+  };
+
   const onChange = (location) => {
     console.log(location);
   };
@@ -27,7 +35,10 @@ const Favourites = () => {
   return (
     <Layout isWithMenu={true}>
       <DropDown onChange={onChange} options={optionsArr} />
-      <Card cardArray={user.favourites} />
+      <Card
+        cardArray={user.favourites}
+        handleRemoveFavourites={handleRemoveFavourites}
+      />
     </Layout>
   );
 };
