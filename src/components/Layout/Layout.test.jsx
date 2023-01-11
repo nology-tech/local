bf6246c7fe7d480764f0a1da5ca0d9e6e2c9ec80
Edgars <Edgars.Kaufmanis@nology.io>
2023-01-11 17:16:57 +0000

    added testing

--- src/components/Layout/Layout.test.jsx
+++ src/components/Layout/Layout.test.jsx
@@ -1,5 +1,6 @@
 import Layout from "./Layout";
 import { customRender } from "../../utils/testUtils";
+import { act } from "react-dom/test-utils";
 
 it("should render the layout component", () => {
   const { container } = customRender(
@@ -9,4 +10,14 @@ it("should render the layout component", () => {
   );
 
   expect(container).toMatchSnapshot();
+});
+
+it("should apply a different class when the menu is open/closed", () => {
+  const { container } = customRender(
+    <Layout isWithMenu={true} showMenu={false} />
+  );
+
+  const menu = container.querySelector(".layout__menu");
+
+  expect(menu).toHaveClass("menu-closed");
 });
