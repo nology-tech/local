053ac7495aabc894d53842baccbfb93fdb9419e0
Edgars <Edgars.Kaufmanis@nology.io>
2023-01-11 15:53:45 +0000

    added sliding animation to menu

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -18,14 +18,11 @@ const Layout = ({ children, isWithMenu }) => {
   return (
     <div className="layout">
       <Header toggleMenu={toggleMenu} />
-      {showMenu && (
-        <>
-          <div className="layout__menu--shadow"></div>
-          <div className="layout__menu">
-            <Menu toggleMenu={toggleMenu} />
-          </div>
-        </>
-      )}
+
+      {showMenu && <div className="layout__menu--shadow"></div>}
+      <div className={`layout__menu ${showMenu ? "menu-open" : "menu-closed"}`}>
+        <Menu toggleMenu={toggleMenu} />
+      </div>
 
       {children}
     </div>
