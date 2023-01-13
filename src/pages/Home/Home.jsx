e99ed35d296e72335f4bd568ea0a23e62943485b
katherine <pv.katherine7@gmail.com>
2023-01-13 11:25:37 +0000

    implementing search by post code

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,26 +2,55 @@ import Button from "../../components/Button/Button";
 import Layout from "../../components/Layout/Layout";
 import Carousel from "../../components/Carousel/Carousel";
 import Card from "../../components/Card/Card";
+import TextField from "../../components/TextField/TextField";
 import { useEffect, useState } from "react";
 import { getInRadius } from "../../api/placeService";
 
 const Home = () => {
   const [businessArr, setBusinessArr] = useState([]);
+  const [searchPostCode, setSearchPostCode] = useState("");
+
   if (businessArr === null) {
     return <p>loading...</p>;
   }
 
-  const getData = async () => {
-    const data = await getInRadius("CH451HE", 5);
-    setBusinessArr(data);
+  const handleSearchInput = (event) => {
+    const useSearchInput = event.target.value;
+    setSearchPostCode(useSearchInput);
   };
+  console.log(searchPostCode);
 
   useEffect(() => {
-    getData();
-  }, []);
+    getData(searchPostCode);
+  }, [searchPostCode]);
+
+  const getData = async (searchPostCode) => {
+    // if (searchPostCode !== "") {
+    const data = await getInRadius({ searchPostCode }, 5);
+    setBusinessArr(data);
+    // }
+  };
+  console.log(businessArr[0]);
 
   return (
     <Layout isWithMenu={true}>
+      <div className="home-list-view__search--bar-container">
+        {/* <img
+          className="home-list-view__map-icon"
+          src={MapIcon}
+          type="image/svg+xml"
+          alt="an icon of a map marker"
+        /> */}
+
+        <TextField
+          uniqueId="location-search-bar"
+          inputType="text"
+          modifier="location-search-bar"
+          placeholderText="Tooting, London, GB"
+          onChange={handleSearchInput}
+        />
+      </div>
+
       <div className="home__map-buttons">
         <Button buttonName="map-navigation-zoom" buttonText="+" />
         <Button buttonName="map-navigation-zoom" buttonText="-" />
