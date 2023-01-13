496ea7f9e8c4443eb37cf931767f4b5ca9657727
george Swift <george.swift@nology.io>
2023-01-13 08:30:36 +0000

    created tests for button linking between map view and list view

--- src/pages/HomeListView/HomeListView.test.jsx
+++ src/pages/HomeListView/HomeListView.test.jsx
@@ -1,7 +1,19 @@
 import { customRender } from "../../utils/testUtils";
 import HomeListView from "./HomeListView";
+import Home from "../Home/Home";
+import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
 it("should render the text on the display", () => {
   const { container } = customRender(<HomeListView />);
 
   expect(container).toMatchSnapshot();
+});
+
+it("should render the home page when a user clicks map-view", () => {
+  customRender(<HomeListView />);
+  const { container } = customRender(<Home />);
+  const mapViewLink = screen.getByText("Map View");
+  userEvent.click(mapViewLink);
+  expect(container).toBeInTheDocument();
 });
