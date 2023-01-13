4e2393ea3c690086bb1174e3cd378a94c9f71a08
george Swift <george.swift@nology.io>
2023-01-13 11:35:36 +0000

    updated tests

--- src/pages/Home/Home.test.jsx
+++ src/pages/Home/Home.test.jsx
@@ -1,8 +1,6 @@
 import Home from "../../pages/Home/Home";
 import { customRender } from "../../utils/testUtils";
 import { screen } from "@testing-library/react";
-import HomeListView from "../HomeListView/HomeListView";
-import userEvent from "@testing-library/user-event";
 
 it("should render a button on the screen", () => {
   customRender(<Home />);
@@ -16,12 +14,4 @@ it("should render the text on the display", () => {
   const { container } = customRender(<Home />);
 
   expect(container).toMatchSnapshot();
-});
-
-it("should render the list view page when a user clicks list view", () => {
-  customRender(<Home />);
-  const { container } = customRender(<HomeListView />);
-  const listViewLink = screen.getByText("List View");
-  userEvent.click(listViewLink);
-  expect(container).toBeInTheDocument();
 });
--- src/pages/HomeListView/HomeListView.test.jsx
+++ src/pages/HomeListView/HomeListView.test.jsx
@@ -1,19 +1,8 @@
 import { customRender } from "../../utils/testUtils";
 import HomeListView from "./HomeListView";
-import Home from "../Home/Home";
-import { screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 
 it("should render the text on the display", () => {
   const { container } = customRender(<HomeListView />);
 
   expect(container).toMatchSnapshot();
-});
-
-it("should render the home page when a user clicks map-view", () => {
-  customRender(<HomeListView />);
-  const { container } = customRender(<Home />);
-  const mapViewLink = screen.getByText("Map View");
-  userEvent.click(mapViewLink);
-  expect(container).toBeInTheDocument();
 });
