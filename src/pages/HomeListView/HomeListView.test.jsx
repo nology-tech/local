660405e686833d14c58fd38ba419be6edd172a58
george Swift <george.swift@nology.io>
2023-01-16 15:03:14 +0000

    deleted home list view folder as no longer used

--- src/pages/HomeListView/HomeListView.test.jsx
+++
@@ -1,8 +0,0 @@
-import { customRender } from "../../utils/testUtils";
-import HomeListView from "./HomeListView";
-
-it("should render the text on the display", () => {
-  const { container } = customRender(<HomeListView />);
-
-  expect(container).toMatchSnapshot();
-});
